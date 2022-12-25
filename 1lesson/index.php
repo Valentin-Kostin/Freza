359af78586fcb8cee8d2cd52483e4a987e82fb32
Valentin <frezer.2021@mail.ru>
2022-12-25 11:50:52 +0300

    redaktirovali papku 1lesson

--- 1lesson/index.php
+++ 1lesson/index.php
@@ -5,5 +5,28 @@
 	echo "<br/>";
 	echo date_default_timezone_get();
 	echo "<br/>";
-	?>
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta content="text/html; charset=Windows-1251"
+           http-equiv="content-type">
+	 <link rel=styleset href=1lesson\styles.css media=screen/>	<!-- это тут не работает-->   
+     <title> С днем рождения </title>
+   </head>
+   <body>
+   <a href=../index.php>На главную</a>
+    <h1> С днем    рождения! </h1> 
+	<p>Дорогой Вася!</p>
+	<p>От <i>всей <b>души поздравляю</i> тебя.</b></p>
+	
+	<img src="2021-11-24_11-41-59 — копия.png"/>
+	
+	
+	<p>Эту открытку я сделал своими руками.</p>
+	<p>ValenTigra</p>
+	<a href=http://www.dummies.com> в интернет </a><br/>
+	<a href=index.php> обратно на страницу </a>
+   </body>
+</html>
 
