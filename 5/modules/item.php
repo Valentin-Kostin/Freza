321805c29d19ff639b07ad838d401ec0702690fa
Valentin <frezer.2021@mail.ru>
2022-12-25 13:22:21 +0300

    nado s 5 razobrat

--- 5/modules/item.php
+++ 5/modules/item.php
@@ -5,16 +5,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link href="/styles.css" rel="stylesheet" type="text/css">
+        <link href="../styles.css" rel="stylesheet" type="text/css">
         <title><?php echo $item['desc']?>: Фотогалерея</title>
     </head>
     <body>
         <h1>Фотогалерея</h1>
 		<h2><?php echo $item['desc']?></h2>
-        <p><a href="../5/index.php">На главную</a></p>
+        <p><a href="../index.php">На главную</a></p>
         <section id="gallery-item">
-		    <img src="../5/images/<?php echo $item['src']?>">
+		    <img src="../images/<?php echo $item['src']?>">
         </section>
-        <p><a href="../5/index.php">На главную</a></p>
+        <p><a href="../index.php">На главную</a></p>
     </body>
 </html>
--- 5/modules/list.php
+++ 5/modules/list.php
@@ -2,18 +2,19 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link href="/5/styles.css" rel="stylesheet" type="text/css">
+        <link href="../styles.css" rel="stylesheet" type="text/css">
         <title>Фотогалерея</title>
     </head>
     <body>
+        <a href="../index.php">На главную</a>
         <h1>Фотогалерея</h1>
         <section id="gallery">
 		<?php 
 		    $cnt = count($arr_images);
 			for ($i = 0; $i<$cnt; $i++)
 			{?>
-				<a href="/5/<?php echo $i ?>/">
-				    <img src = "/5/images/<?php echo $arr_images[$i]['src'];?>"
+				<a href="../<?php echo $i ?>/">
+				    <img src = "../images/<?php echo $arr_images[$i]['src'];?>"
 				    title="<?php echo $arr_images[$i]['desc']?>">
 				</a>
 			<?php }
