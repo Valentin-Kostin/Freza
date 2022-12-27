b1da3f405d7146ecae2d5759d6b346ea9487b1a4
Valentin <frezer.2021@mail.ru>
2022-12-27 21:50:44 +0300

    stepik2.4

--- Stepik 2/stepik.js
+++ Stepik 2/stepik.js
@@ -4,12 +4,27 @@ function myFunction(login) {
         console.log("Администрирование, Операции, Статистика, Роли.")
     }
     else if (login === "support"){
-        console.log("Операции, Статистика.")
+        console.log("Операции, Статистика.");
     }
     else if (login === "user"){
-        console.log("Статистика.")
+        console.log("Статистика.");
     }
     else{
-        console.log("Неверный логин")
+        console.log("Неверный логин");
     }
+    alert(login);
 }
+function myFunction2(check) {
+    if(check >= 750)
+    {console.log(parseInt(check*0.95))}
+else{console.log(parseInt(check))}
+};
+
+
+function myFunction3(n=3, s=50) {    
+    let totalprice=0;
+    for (let i=0; i<=n; i++) {
+      totalprice = s*i;
+  }
+  console.log(totalprice)
+  }
